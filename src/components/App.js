4bd38f6b6eb6f221c3bc13a5331de717973f5fe2
Daniel Zen <github@zendigital.com>
2019-07-19 14:01:55 -0400

    create-react-app TodoMVC graphcool

---
+++ src/components/App.js
@@ -0,0 +1,21 @@
+import ApolloClient from 'apollo-boost'
+import { ApolloProvider } from 'react-apollo'
+import React from 'react';
+import Header from './Header'
+import MainSection from './MainSection'
+
+const client = new ApolloClient({
+  uri: 'https://api.graph.cool/simple/v1/cjyae8t3u1tej0159p6f6vcx7'
+});
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <div>
+      <Header />
+      <MainSection />
+    </div>
+  </ApolloProvider>
+);
+
+export default App
+
